ec435601fc90b56064b66c338382824e96276c2f
yusbel86 <yusbel1086@gmail.com>
2020-02-20 01:44:25 +0000

    Added the Card

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Card } from "./card";
 //create your first component
 export function Home() {
 	const [people, setPeople] = useState([]);
 
 	useEffect(() => {
-		fetch("https://swapi.co/api/people")
+		fetch("https://swapi.co/api/films/")
 			.then(Response => Response.json())
 			.then(data => {
 				console.log(data.results);
@@ -19,7 +19,14 @@ export function Home() {
 	return (
 		<div className="text-center">
 			{people.map((item, index) => {
-				return <div key={index}>{item.count}</div>;
+				return (
+					<Card
+						key={index}
+						title={item.title}
+						dir={item.director}
+						des={item.opening_crawl}
+					/>
+				);
 			})}
 		</div>
 	);
